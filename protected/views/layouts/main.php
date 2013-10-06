739f017d381537ad03df716510363cec4c812c25
Евгений Питиков <evgeniy@desktop.pitikov>
2013-10-06 12:38:15 +0400

    упс - забыл, исправляюсь

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,4 +1,5 @@
 <?php /* @var $this Controller */ ?>
+<?php /*
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
@@ -15,7 +16,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	
 </head>
 
 <body>
@@ -55,6 +56,71 @@
 	</div><!-- footer -->
 
 </div><!-- page -->
+
+</body>
+</html>
+*/?>
+<!DOCTYPE html>
+<html lang="ru" xml:lang="ru">
+    <head>
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta http-equiv="content-language" content="ru" />
+	<meta name="author" content="Ezersky Boris" />
+
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	
+	<meta name="robots" content="index,follow" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta http-equiv="cache-control" content="no-cache">
+	
+	<link rel="Shortcut Icon" type="image/x-icon" href="favicon.ico" size="16x16"/>
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/alpsite.css" media="screen, projection" />
+
+<style media="handheld">
+</style>
+
+<style media="print">
+</style>
+
+<script type="text/javascript">
+<!--	
+-->
+</script>
+
+</head>
+<body>
+	<header>
+		<div id="logo"></div>
+		<nav id="navigation" role="navigation">
+		    <?php
+			echo CHtml::link('федерация',$this->createUrl('/federation'));
+			echo CHtml::link('клуб',$this->createUrl('/mountaineeringclub'));
+			echo CHtml::link('события',$this->createUrl('/events'));
+			echo CHtml::link('отчёты',$this->createUrl('/reports'));
+			echo CHtml::link('горы мира',$this->createUrl('/mountains'));
+		    ?>
+		</nav>
+	</header>
+
+	<aside>
+		<nav role="toolbar search">
+		</nav>
+	</aside>
+	
+	<section id="main-section" role="main">
+	    <?php 
+		// тут собственно происходит вставка содержимого
+		echo $content; 
+	    ?>
+	</section>
+
+	<footer role="contentinfo">
+		<address>
+		</address>
+	</footer>
 
 </body>
 </html>
