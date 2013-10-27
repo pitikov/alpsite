f6ffe7a103cd255e984ff100bc84b388e89cccf3
Евгений Питиков <pitikov@yandex.ru>
2013-10-27 11:22:35 +0400

    Добавил кнопки соц-сетей для статьи

--- protected/views/layouts/article.php
+++ protected/views/layouts/article.php
@@ -4,15 +4,42 @@
 <div id="content">
 	<?php echo $content; ?>
 </div><!-- content -->
-<div id="socialuttons">
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
+<div id="socialuttons" style="display:block;">
+    <!-- Facebook-->
+    <div id="beSocial"  style="display:inline-block;">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-    <div class="fb-like" data-href="http://alpsite58.ru" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="box_count" data-action="like" data-show-faces="true" data-send="false"></div>
+<div class="fb-like" data-href="http://alpsite58.ru" data-width="The pixel width of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
+
+<!-- Put this script tag to the <head> of your page -->
+<script type="text/javascript" src="http://vk.com/js/api/share.js?86" charset="windows-1251"></script>
+    </div>
+    <div id="beSocial"  style="display:inline-block;">
+<!-- Put this script tag to the place, where the Share button will be -->
+<script type="text/javascript"><!--
+document.write(VK.Share.button(false,{type: "round", text: "Нравится"}));
+--></script>
 </div>
+    <div id="beSocial" style="display:inline-block;">
+        <!-- Place this tag where you want the +1 button to render. -->
+<div class="g-plusone" data-annotation="inline" data-width="300"></div>
+
+<!-- Place this tag after the last +1 button tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script></div>
+
+</div>
+
+
 <?php $this->endContent(); ?>
